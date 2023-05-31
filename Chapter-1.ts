c689b72bc7f434418b5f0b292de2a7dc3ca754af
SergioBurlaka <burlaka01@gmail.com>
2023-05-31 17:12:14 +0300

    types from array and enum

---
+++ Chapter-1.ts
@@ -0,0 +1,22 @@
+
+
+// Лінк на туторіал
+
+// https://www.youtube.com/watch?v=gieEQFIfgYc&t=5s&ab_channel=DaveGray
+// ts-node ./Chapter-1.ts
+
+
+///
+
+const a = 12
+const b = '6'
+const c = 3
+
+
+const adding = (arg1: number, arg2: number) => arg1 + arg2
+
+const regEx: RegExp = /\w+/g
+
+const regExForTelephoneNumber: RegExp = /^\d{3}\d{3}\d{4}$/
+
+
--- tut-4.ts
+++ tut-4.ts
@@ -3,7 +3,7 @@
 // Лінк на туторіал
 
 // https://www.youtube.com/watch?v=_oilipTMJ5Y&list=PLiZoB8JBsdzlG1oAY8U4vrBtVW07j6jil&index=5&ab_channel=%D0%9C%D0%B8%D1%85%D0%B0%D0%B8%D0%BB%D0%9D%D0%B5%D0%BF%D0%BE%D0%BC%D0%BD%D1%8F%D1%89%D0%B8%D0%B9
-// ts-node ./tut-3.ts
+// ts-node ./tut-4.ts
 
 
 ///
@@ -83,3 +83,21 @@ getLength_2([7, 10, 'sdg', 'hello'])
 
 // getLength(true)
 //Argument of type 'boolean' is not assignable to parameter of type 'TypeEithLenght'.ts(2345)
+
+
+const getkey = <T, K extends keyof T>(obj: T, key: K) => {
+  return obj[key]
+}
+
+const ford = {
+  label: 'Ford',
+  price: 2500
+}
+
+// Можна передати поле тільки яке є у об'єкті
+
+const res_2 = getkey(ford, 'label')
+
+// const res_3 = getkey(ford, 'name') 
+// Argument of type '"name"' is not assignable to parameter of type '"label" | "price"'.ts(2345)
+
