6c3a19b387bdcb01998e831245c6cd364d518cd8
SergioBurlaka <burlaka01@gmail.com>
2023-06-07 23:39:15 +0300

    object loop and type for key using Record

--- Chapter-8.ts
+++ Chapter-8.ts
@@ -68,10 +68,39 @@ const student: Student = {
 // перебір пропсів обєкту та іх тип
 
 
-for (const key in student){
+for (const key in student) {
   console.log('student[key]', student[key as keyof Student])
 }
 
-Object.keys(student).map(key =>{
+Object.keys(student).map(key => {
   console.log(student[key as keyof typeof student])
 })
+
+
+///////
+
+
+const logStudentKey = (student: Student, key: keyof Student): void => {
+  console.log(`Student ${key}: ${student[key]}`)
+}
+
+logStudentKey(student, 'name')
+
+
+//////////////////////
+
+
+type Streams = 'salary' | 'bonus' | 'sidehustle'
+
+type Incomes = Record<Streams, number | string>
+
+
+const monthlyIncomes: Incomes = {
+  salary: 500,
+  bonus: 100,
+  sidehustle: 250
+}
+
+for (const revenue in monthlyIncomes) {
+  console.log(monthlyIncomes[revenue as keyof Incomes])
+}
