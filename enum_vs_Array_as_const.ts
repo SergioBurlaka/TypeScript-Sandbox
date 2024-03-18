715ad3abb1c9cdacab4308f21be023952715151a
burlaka sergii <burlaka01@gmail.com>
2024-03-19 00:58:30 +0200

    enum vs contant from array

--- enum_vs_Array_as_const.ts
+++ enum_vs_Array_as_const.ts
@@ -1,3 +1,4 @@
+// ts-node ./enum_vs_Array_as_const.ts
 enum Satus {
   success = "Success",
   error = "Error",
@@ -13,16 +14,42 @@ const response: myTypeWithGeneric<{}> = {
   data: {},
 };
 
-const statusFromArr = ["Success", "Error"] as const;
+const statusFromArr = ["Success", "Error", "Loading"] as const;
 
 type StatusEnumFromErray = (typeof statusFromArr)[number];
 
 type myTypeWithGeneric_2<T> = {
   status: StatusEnumFromErray;
-  data: T;
+  data?: T;
 };
 
 const responseObj: myTypeWithGeneric_2<{}> = {
   status: "Success",
   data: {},
 };
+
+const responses = [
+  { status: "Success" },
+  { status: "Error" },
+  { status: "Loading" },
+];
+
+console.log("responses", responses);
+
+function printLocation_2(locationState: myTypeWithGeneric_2<never>) {
+  switch (locationState.status) {
+    case "Success":
+      console.log("Success", locationState.status);
+      break;
+    case "Error":
+      console.log("Error", locationState.status);
+      break;
+    case "Loading":
+      console.log("Loading", locationState.status);
+      break;
+  }
+}
+
+responses.forEach((item) =>
+  printLocation_2(item as myTypeWithGeneric_2<never>)
+);
